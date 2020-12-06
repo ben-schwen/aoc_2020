36c5d874d31975b770f814df7af24040b7320487
ben-schwen <e1225371@student.tuwien.ac.at>
2020-12-06 18:54:45 +0100

    refined day5

--- aoc5.pl
+++ aoc5.pl
@@ -17,68 +17,42 @@ row_col_id(Row, Col, Id) :-
 	Row in 0..127,
 	Col in 0..7.
 
-list_maximum([L|Ls], M) :-
-	foldl(maximum_, Ls, L, M).
-maximum_(L, M0, M) :- M #= max(L,M0).
+seat_next(L0, U0, C, L, U) :-
+	(C = 'F'; C = 'L'),
+	L #= L0,
+	U #= L0 + ((U0 - L0) // 2).
+seat_next(L0, U0, C, L, U) :-
+	(C = 'B'; C = 'R'),
+	L #= U0 - ((U0 - L0) // 2),
+	U #= U0.
 
-list_minimum([L|Ls], M) :-
-	foldl(minimum_, Ls, L, M).
-minimum_(L, M0, M) :- M #= min(L,M0).
+seat(L, U, [], L, U).
+seat(L0, U0, [C|Cs], L, U) :-
+	seat_next(L0, U0, C, L1, U1),
+	seat(L1, U1, Cs, L, U).
 
-seat(Lower, Upper, Code, L1, U1) :-
-	(Code = 'F'; Code = 'L'),
-	L1 #= Lower,
-	U1 #= Lower + ((Upper - Lower) // 2).
-seat(Lower, Upper, Code, L1, U1) :-
-	(Code = 'B'; Code = 'R'),
-	L1 #= Upper - ((Upper - Lower) // 2),
-	U1 #= Upper.
-
-/* modularised version for arbitrary big planes
-find_row([], 0, 127).
-find_row([C|Cs], L1, U1) :-
-	seat(L, U, C, L1, U1),
-	find_row(Cs, L, U).
-
-find_col([], 0, 7).
-find_col([C|Cs], L1, U1) :-
-	seat(L, U, C, L1, U1),
-	find_col(Cs, L, U).
-
-% https://stackoverflow.com/questions/27151274/prolog-take-the-first-n-elements-of-a-list
-take(N, _, Xs) :- N =< 0, !, N =:= 0, Xs = [].
-take(_, [], []).
-take(N, [X|Xs], [X|Ys]) :- M is N-1, take(M, Xs, Ys).
+split_at(N,Xs,Take,Rest) :-
+    split_at_(Xs,N,Take,Rest).
+split_at_(Rest, 0, [], Rest) :- !. % optimization
+split_at_([], N, [], []) :- N > 0.
+split_at_([X|Xs], N, [X|Take], Rest) :-
+    N > 0,
+    succ(N0, N),
+    split_at_(Xs, N0, Take, Rest).
 
 description_id(Cs,Id) :-
-	take(7, Cs, Row_C),
-	reverse(Row_C, Rows),
-	reverse(Cs, Rev_Cs),
-	take(3, Rev_Cs, Cols),
-	find_row(Rows, Row, Row),
-	find_col(Cols, Col, Col),
-	row_col_id(Row, Col, Id).*/
-
-description_id([C1,C2,C3,C4,C5,C6,C7,C8,C9,C10],Id) :-
-	seat(0, 127, C1, L1, U1),
-	seat(L1, U1, C2, L2, U2),
-	seat(L2, U2, C3, L3, U3),
-	seat(L3, U3, C4, L4, U4),
-	seat(L4, U4, C5, L5, U5),
-	seat(L5, U5, C6, L6, U6),
-	seat(L6, U6, C7, Row, Row),
-	seat(0, 7, C8, L8, U8),
-	seat(L8, U8, C9, L9, U9),
-	seat(L9, U9, C10, Col, Col),
+	split_at(7, Cs, C_Rows, C_Cols),
+	seat(0, 127, C_Rows, Row, Row),
+	seat(0, 7,   C_Cols, Col, Col),
 	row_col_id(Row, Col, Id).
 
 main :-
 	read_file('input5', Input), 
 	maplist(maplist(char_code), Cs, Input),
 	maplist(description_id, Cs, Ids),
-	list_maximum(Ids, Max),
+	max_list(Ids, Max),
 	writeln(Max),
-	list_minimum(Ids, Min),
+	min_list(Ids, Min),
 	numlist(Min, Max, L),
 	subtract(L, Ids, Seat),
 	writeln(Seat).
