6cfc4ff90d0cb4caada43e51022ed00096ebd59f
ben-schwen <e1225371@student.tuwien.ac.at>
2020-12-18 17:04:33 +0100

    finished day14

--- day14/aoc14.pl
+++ day14/aoc14.pl
@@ -2,7 +2,7 @@
 :- use_module(library(dcg/basics)).
 :- set_prolog_flag(double_quotes, codes).
 :- use_module(library(clpfd)).
-:- use_module(library(reif)).
+:- use_module(library(yall)).
 
 mask([]) --> [].
 mask([_|Xs]) --> "X", mask(Xs).
@@ -27,8 +27,6 @@ binlist_dec([B|Bs], I0, N0, N) :-
         I1 #= I0 + 1,
         binlist_dec(Bs, I1, N1, N).
 
-number_code(N, C) :- number_codes(N, [C]).
-
 dec_bin(Dec, Bin) :-
 	N = 36,
 	binlist_dec(Ns, Dec),
@@ -37,41 +35,86 @@ dec_bin(Dec, Bin) :-
 	append(Zeroes, Ns, Bin), 
 	!.
 
-mask_one(_, Mask, Mask) :-
-	ground(Mask).
-mask_one(Val, Mask, Val) :-
-	var(Mask).
+mask1(_, Mask, Mask) :- ground(Mask).
+mask1(Val, Mask, Val) :- var(Mask).
 
-mask_all(Val, Mask, All) :-
-	maplist(mask_one, Val, Mask, All).	
-
-list_index_el(Ls, I, El) :- nth1(I, Ls, El).
+mask_all(Val, Mask, All) :- maplist(mask1, Val, Mask, All).	
 
 mask_val_dec(Mask, Val, Dec) :-
 	dec_bin(Val, Bin),
 	mask_all(Bin, Mask, BDec),
 	binlist_dec(BDec, Dec).
 
+task1_(Mask-Mem, DictIn, DictOut) :-	
+	pairs_values(Mem, Vs),
+	pairs_keys(Mem, Is),
+	maplist(mask_val_dec(Mask), Vs, Ds),
+	pairs_keys_values(Ps, Is, Ds),
+	put_dict(Ps, DictIn, DictOut).
+
+task1 :-
+	read_file('input14', Input), 
+	foldl(task1_, Input, ht{}, HT), 
+	dict_keys(HT, Keys), 
+	maplist({HT}/[Key,Val]>>get_dict(Key,HT,Val), Keys, Vals),
+	sumlist(Vals, Sum),
+	writeln(Sum).
+
+mask2(_, Mask, Mask) :- var(Mask), !.
+mask2(Val, 0, Val).
+mask2(_, 1, 1).
+
+float_dec(Float, Dec) :-
+	Float ins 0..1,
+	include(var, Float, Vars),
+	label(Vars),
+	binlist_dec(Float, Dec).
+
+float_all(Float, Ds) :- findall(Dec, float_dec(Float, Dec), Ds).
+
+mask_val_float(Mask, Val, Fs) :-
+	dec_bin(Val, Bin),
+	maplist(mask2, Bin, Mask, Float),
+	float_all(Float, Fs).
+
+repeat(X, N, Xs) :- length(Xs, N), maplist(=(X), Xs).
+
 list_uniqueIndex(List, UI) :-
 	must_be(list, List),
 	lists:number_list(List, 1, Rev),
 	reverse(Rev, Numbered),
-    % fisrt sort by value in order to remove duplicates
     sort(1, @=<, Numbered, ONum),
     lists:remove_dup_keys(ONum, NumSet),
     pairs_values(NumSet, UI).
 
-task1_(Mask-Mem, Is, Ds) :-	
+list_select(Xs, Select, Xs_Select) :-
+	maplist({Xs}/[I,E] >> nth1(I, Xs, E), Select, Xs_Select).
+
+keys_val_pairs(Keys, Val, Pairs) :-
+	maplist(length, Keys, Lens),
+	maplist(repeat, Val, Lens, Vs),
+	flatten(Vs, VsFlat),
+	flatten(Keys, KsFlat),
+	list_uniqueIndex(KsFlat, Unique),
+	list_select(KsFlat, Unique, Ks_Uniq),
+	list_select(VsFlat, Unique, Vs_Uniq),
+	pairs_keys_values(Pairs, Ks_Uniq, Vs_Uniq).
+
+task2_(Mask-Mem, DictIn, DictOut) :-
 	pairs_values(Mem, Vs),
 	pairs_keys(Mem, Is),
-	maplist(mask_val_dec(Mask), Vs, Ds).
+	maplist(mask_val_float(Mask), Is, As),
+	keys_val_pairs(As, Vs, Ps),
+	put_dict(Ps, DictIn, DictOut).
+
+task2 :-
+	read_file('input14', Input), 
+	foldl(task2_, Input, ht{}, HT), 
+	dict_keys(HT, Keys), 
+	maplist({HT}/[Key,Val]>>get_dict(Key,HT,Val), Keys, Vals),
+	sumlist(Vals, Sum),
+	writeln(Sum).
 
 main :-
-	read_file('input14', Input),
-	maplist(task1_, Input, Iss, Dss),
-	flatten(Iss, Is),
-	flatten(Dss, Ds),
-	list_uniqueIndex(Is, Unique),
-	maplist(list_index_el(Ds), Unique, Ss),
-	sumlist(Ss, Sum),
-	writeln(Sum).
+	task1,
+	task2.
